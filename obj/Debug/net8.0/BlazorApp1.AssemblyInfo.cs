912d72068de893eeeb50c1cb885a7c229f05b049
Amandi <119551535+nimashaklaa@users.noreply.github.com>
2025-12-27 12:11:49 +0530

    Update AssemblyInfo with new informational version and clean up cache

--- obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba1d2a96a25345091dc39c1f9ca88dbe7a9a72c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+828c4c9f9dc7410dc199dad1c20687226479ecd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
